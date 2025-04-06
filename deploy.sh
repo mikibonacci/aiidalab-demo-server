a96850ffbc7d442e9f840d3187f71fb404d3907b
Miki Bonacci <miki.bonacci@psi.ch>
2025-04-06 14:21:52 +0200

    adding the possibility to define list of allowed users

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo ${K8S_NAMESPACE}
+
 helm dependency build basehub 2> /dev/null || echo "No dependencies to update or install"
+
+jinja2 --format=env jinja_templates/storageclass.yaml.j2 > basehub/templates/storageclass.yaml
+jinja2 --format=env jinja_templates/values.yaml.j2 > basehub/values.yaml
+
+cp allowed_users_lists/${K8S_NAMESPACE}.yaml basehub/files/allowed_users_list.yaml
 
 # JupyterHub
 helm upgrade \
